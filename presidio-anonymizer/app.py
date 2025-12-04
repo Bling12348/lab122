4dded793dfb9343471dcb9a52d1aeb5295d2a909
Tony Schembri <230397822+Bling12348@users.noreply.github.com>
2025-12-04 05:56:48 +0000

    Add /genz-preview endpoint to Presidio-Anonymizer and was able to get successfuly API call

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -94,8 +94,7 @@ class Server:
         @self.app.route("/deanonymizers", methods=["GET"])
         def deanonymizers():
             """Return a list of supported deanonymizers."""
-            return jsonify(self.deanonymize.get_deanonymizers())
-        
+            return jsonify(self.deanonymize.get_deanonymizers())        
         @self.app.route("/genz-preview", methods=["GET"])
         def genz_preview():
             response = {
